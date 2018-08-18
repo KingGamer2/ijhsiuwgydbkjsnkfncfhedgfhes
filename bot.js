49f9361a3f22c5861b6fb13f7d4ab8bc544de91c
KingGamer2 <40805903+KingGamer2@users.noreply.github.com>
2018-08-18 16:26:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,48 +7,18 @@ console.log("Welcome Again !");
   member.createDM().then(function (channel) {
   return channel.send(` 
 **
-حياك الله في سيرفرنا[10+ People Invited]
-➤ [:page_with_curl:] Tier I
-➤ [:money_with_wings:] $20 Paypal
-
-[20+ People Invited]
-➤ [:page_with_curl:] Tier II
-➤ [:money_with_wings:] $45 Paypal
-
-[30+ People Invited]
-➤ [:page_with_curl:] Tier III
-➤ [:money_with_wings:] $70 Paypal
-
-[40+ People Invited]
-➤ [:page_with_curl:] Tier IV
-➤ [:money_with_wings:] $95 Paypal
-
-[50+ People Invited]
-➤ [:page_with_curl:] Tier V
-➤ [:money_with_wings:] $120 Paypal
-
-[60+ People Invited]
-➤ [:page_with_curl:] Tier VI
-➤ [:money_with_wings:] $145 Paypal
-
-[70+ People Invited]
-➤ [:page_with_curl:] Tier VII
-➤ [:money_with_wings:] $170 Paypal
-
-**[80+ People Invited]
-➤ [:page_with_curl:] Tier VIII
-➤ [:money_with_wings:] $195 Paypal
-
-[90+ People Invited]
-➤ [:page_with_curl:] Tier IX
-➤ [:money_with_wings:] $220 Paypal
-
-[100+ People Invited]
-➤ [:page_with_curl:] Tier X
-➤ [:money_with_wings:] $250 Paypal
-https://discord.gg/CkV8ytG
+اول موقع عربي لحسابات ماين كرافت 
+فل اكسس | شبه فل داتا | عادية 
+مجانا" !!؟؟
+http://www.arabalts.tk/
+========================================
+[Hello i make a free minecraft alts web
+full Access | Semi Full Data | Normal
+Free 100% I hope you like it and support me by posting
+to post more and more accounts]
+http://www.arabalts.tk
  ... [ ${member}  ]
-**`) 
+**`)
 }).catch(console.error)
 
 })
@@ -57,46 +27,16 @@ client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
 **
-حياك الله في سيرفرنا[10+ People Invited]
-➤ [:page_with_curl:] Tier I
-➤ [:money_with_wings:] $20 Paypal
-
-[20+ People Invited]
-➤ [:page_with_curl:] Tier II
-➤ [:money_with_wings:] $45 Paypal
-
-[30+ People Invited]
-➤ [:page_with_curl:] Tier III
-➤ [:money_with_wings:] $70 Paypal
-
-[40+ People Invited]
-➤ [:page_with_curl:] Tier IV
-➤ [:money_with_wings:] $95 Paypal
-
-[50+ People Invited]
-➤ [:page_with_curl:] Tier V
-➤ [:money_with_wings:] $120 Paypal
-
-[60+ People Invited]
-➤ [:page_with_curl:] Tier VI
-➤ [:money_with_wings:] $145 Paypal
-
-[70+ People Invited]
-➤ [:page_with_curl:] Tier VII
-➤ [:money_with_wings:] $170 Paypal
-
-**[80+ People Invited]
-➤ [:page_with_curl:] Tier VIII
-➤ [:money_with_wings:] $195 Paypal
-
-[90+ People Invited]
-➤ [:page_with_curl:] Tier IX
-➤ [:money_with_wings:] $220 Paypal
-
-[100+ People Invited]
-➤ [:page_with_curl:] Tier X
-➤ [:money_with_wings:] $250 Paypal
-https://discord.gg/CkV8ytG
+اول موقع عربي لحسابات ماين كرافت 
+فل اكسس | شبه فل داتا | عادية 
+مجانا" !!؟؟
+http://www.arabalts.tk/
+========================================
+[Hello i make a free minecraft alts web
+full Access | Semi Full Data | Normal
+Free 100% I hope you like it and support me by posting
+to post more and more accounts]
+http://www.arabalts.tk
  ... [ ${member}  ]
 **`)
 }).catch(console.error)
