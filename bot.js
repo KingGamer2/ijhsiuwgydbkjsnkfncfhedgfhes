1b6f270aadf7146f98a50a760a1c2d1ed31a2ebc
KingGamer2 <40805903+KingGamer2@users.noreply.github.com>
2018-09-11 19:57:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -143,7 +143,6 @@ client.on('message', message => {
   if(!message.channel.guild) return;
 if(message.content.startsWith('!ms')) {
 if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
 let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
 let copy = "KinGBot";
 let request = `Requested By ${message.author.username}`;
